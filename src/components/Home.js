a7bc8014770ee4bb65ea60659cf6bb78f4f0a22c
the-real-ihollander <ian.hollander+curriculumtest@flatironschool.com>
2021-05-26 07:50:05 -0400

    commit tree fcb0e4fd90b8c34d390d90c76858c7424a238fad

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,16 @@
 import React from "react";
 
-function Home(props) {
-  return (
-    <div id="home">
-      <h1 style={{ color: props.color }}>
-        {props.username} is a Web Developer from {props.city}
-      </h1>
-    </div>
-  );
+class Home extends React.Component {
+  render() {
+    const { props } = this;
+    return (
+      <div id="home">
+        <h1 style={{ color: props.color }}>
+          {props.username} is a Web Developer from {props.city}
+        </h1>
+      </div>
+    );
+  }
 }
 
 export default Home;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,12 +1,13 @@
 import React from "react";
 
-function NavBar() {
-  return (
-    <nav>
-      <a href="#home">Home</a>
-      <a href="#about">About</a>
-    </nav>
-  );
+class About extends React.Component {
+  render() {
+    return (
+      <nav>
+        <a href="#home">Home</a>
+        <a href="#about">About</a>
+      </nav>
+    );
+  }
 }
-
-export default NavBar;
+export default About;
